9e0e8e3208284af250157ce6cf191cc1bbfd542a
Priyanshi-Dhawan <78725801+Priyanshi-Dhawan@users.noreply.github.com>
2022-04-29 17:21:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcamm.set ({
+    width:350
+    height:300
+    image_format='png'
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+
+webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = <img id="captured_image" src='+data_uri+'/>
+
+}
+console.log ('ml5 version',ml5 version)
+
+classifier = m5.imageClassifier('link',model loaded)
+
+function model loaded() {
+    console.log('model loaded');
+
+}
